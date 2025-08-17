e75b0c2fd776f06f2f4217026e18310203157ac0
Deepesh Belani <60230543+QGDeep@users.noreply.github.com>
2025-08-17 23:29:45 +0400

    Add files via upload

---
+++ Source/HexGridGenerator/HexGridGenerator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HexGridGenerator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HexGridGenerator, "HexGridGenerator" );
